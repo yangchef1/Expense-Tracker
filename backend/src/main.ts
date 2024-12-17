e8fd7eefc98eacf41fa859e0ed5d14f8d25520fe
yang <hc1489@naver.com>
2024-12-18 00:20:02 +0900

    feat: 서버 실행 진입점(main.ts) 구현

---
+++ backend/src/main.ts
@@ -0,0 +1,24 @@
+import { ApolloServer } from "apollo-server";
+import { connectDB } from "./db/db.config";
+import { loadFiles } from "@graphql-tools/load-files";
+import { expenseResolvers } from "./resolver/expense.resolver";
+
+async function startServer() {
+  connectDB();
+
+  const typeDefs = await loadFiles("./src/schema/*.graphql");
+
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers: {
+      Query: expenseResolvers.Query,
+      Mutation: expenseResolvers.Mutation,
+    },
+  });
+
+  server.listen(4000).then(({ url }) => {
+    console.log(`Server ready at ${url}`);
+  });
+}
+
+startServer();
