a6c94c7c47f36a28fd6554dd8982948cec8f3f56
yang <hc1489@naver.com>
2024-12-22 01:59:33 +0900

    setting: 프론트엔드 React 세팅

---
+++ frontend/src/reportWebVitals.ts
@@ -0,0 +1,13 @@
+import { onCLS, onFCP, onLCP, onTTFB, onINP } from "web-vitals";
+
+const reportWebVitals = (onPerfEntry?: (metric: any) => void) => {
+  if (onPerfEntry && onPerfEntry instanceof Function) {
+    onCLS(onPerfEntry);
+    onFCP(onPerfEntry);
+    onLCP(onPerfEntry);
+    onTTFB(onPerfEntry);
+    onINP(onPerfEntry);
+  }
+};
+
+export default reportWebVitals;
